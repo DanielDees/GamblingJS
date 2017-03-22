7a8a1ae2bf5a111361bdbd85eca50f2d741e9186
DanielDees <explorers2010@hotmail.com>
2017-03-21 21:21:01 -0400

    Now using JQuery

--- JS/update.js
+++ JS/update.js
@@ -5,32 +5,32 @@ function Casino_DOM_Updater() {
 	//Get the Casino Info
 	this.setCasinoInfo = function() {
 		
-		document.getElementById("odds").innerHTML = "Odds: " + casino.odds + "%";
-		document.getElementById("min-bet").innerHTML = "Min Bet: " + casino.minBet;
-		document.getElementById("payout-rate").innerHTML = "Payout Rate: " + casino.payoutRate + "x";
+		$("#odds").html("Odds: " + casino.odds + "%");
+		$("#min-bet").html("Min Bet: " + casino.minBet);
+		$("#payout-rate").html("Payout Rate: " + casino.payoutRate + "x");
 	};
 
 	//Get Gambling Info
 	this.setGambleInfo = function() {
 		
-		document.getElementById("bank").innerHTML = "Bank: " + player.bank.toFixed(2);
-		document.getElementById("multiplier").innerHTML = "Bet Multi: " + player.betMulti + "x";
-		document.getElementById("max-safe-bets").innerHTML = "Max Safe Bets: " + getMaxSafeBets();
-		document.getElementById("max-safe-bets-odds").innerHTML = "Max Safe Bets Odds: " + getMaxSafeBetsOdds() + "%";
+		$("#bank").html("Bank: " + player.bank.toFixed(2));
+		$("#multiplier").html("Bet Multi: " + player.betMulti + "x");
+		$("#max-safe-bets").html("Max Safe Bets: " + getMaxSafeBets());
+		$("#max-safe-bets-odds").html("Max Safe Bets Odds: " + getMaxSafeBetsOdds() + "%");
 	};
 
 	//Get Player Info
 	this.setPlayerInfo = function() {
 
-		document.getElementById("bet").innerHTML = "Bet: " + player.bet.toFixed(2) + " (Payout: " +  (player.bet + (player.bet * casino.payoutRate)).toFixed(2) + ")";
-		document.getElementById("investment").innerHTML = "Round Investment: " + player.investment.toFixed(2);
-		document.getElementById("highestBet").innerHTML = "Highest Bet: " + player.highestBet.toFixed(2);
+		$("#bet").html("Bet: " + player.bet.toFixed(2) + " (Payout: " +  (player.bet + (player.bet * casino.payoutRate)).toFixed(2) + ")");
+		$("#investment").html("Round Investment: " + player.investment.toFixed(2));
+		$("#highestBet").html("Highest Bet: " + player.highestBet.toFixed(2));
 	};
 
 	//Update the DOM Info
 	this.update = function() {
 
-		document.getElementById("day").innerHTML = "Round: " + casino.round;
+		$("#day").html("Round: " + casino.round);
 
 		//Get Casino Info
 		this.setCasinoInfo();
